c91603abcd679652cae95a548a8ad0bec10514d6
Ivan <ivan.mustonen@helsinki.fi>
2015-08-06 13:59:30 +0300

    Rajanpinta myöhemmin toteutettaville iteraattoireille

---
+++ Fraktaaligeneraattori/src/main/java/Iteraattorit/Iteraattori.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Iteraattorit;
+
+import Kompleksiluku.Kompleksiluku;
+
+/**
+ *
+ * @author Ivan
+ */
+public interface Iteraattori {
+    
+    public int iteroi(Kompleksiluku iteroitavaLuku, int iteraatioRaja, int potenssi);
+    
+}
