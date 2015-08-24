350a2875fe5fe6e03a5645bac5ec8fffab838e08
Ivan <ivan.mustonen@helsinki.fi>
2015-08-24 15:42:04 +0300

    Tarkkailee päivitysnapin painalluksia

---
+++ Fraktaaligeneraattori/src/main/java/ListeneritNappaimistolle/TallennusnapinListeneri.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ListeneritNappaimistolle;
+
+import FraktaalinPiirtoalusta.FraktaalinPiirtoalusta;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Ivan
+ */
+public class TallennusnapinListeneri implements ActionListener{
+    
+    private final FraktaalinPiirtoalusta alusta;
+    
+    public TallennusnapinListeneri(FraktaalinPiirtoalusta alusta) {
+        this.alusta=alusta;
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        alusta.tallennaKuva();
+    }
+    
+}
