02c84281ad36b8a33ac957237e4b409ca889a2f1
Ivan <ivan.mustonen@helsinki.fi>
2015-08-28 19:25:58 +0300

    Rivien sisennys

--- Fraktaaligeneraattori/src/main/java/Fraktaaligeneraattori/App.java
+++ Fraktaaligeneraattori/src/main/java/Fraktaaligeneraattori/App.java
@@ -1,18 +1,15 @@
 package Fraktaaligeneraattori;
 
 import GraafinenKayttoliittyma.GraafinenKayttoliittyma;
-import java.util.Scanner;
 import javax.swing.SwingUtilities;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(new GraafinenKayttoliittyma());
     }
-    
 }
